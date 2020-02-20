9a2d5d06fb4bc7451c32dd041741530e8e9b1efe
Laura <laura.heddleston@gmail.com>
2020-02-20 12:08:23 -0800

    Fix display of grocery list and add more styling

--- app.js
+++ app.js
@@ -1,58 +1,51 @@
-/* TODO
-Inside app.js, create a GroceryList component that contains an unordered 
-list of 2 grocery items. Render this component to the div tag in index.html 
-with an id of app
-*/
-var GroceryList = (props) => {
-  var css = `
-    .groceryList:hover {
-	font-weight: bold;
-	font-color: red;
-	}
-  `;
-  return (
-    <ul>
-	  <style>
-		  {css}
-	  </style>	  
-	  {
-	  props.list.map((key, item) => (
-			<GroceryListItem key={key} list={item} />
-	  ))
-	  }	
-	</ul>
-	);
-};
-
-var App = () => (
-	<div>
-		<h2>Grocery List</h2>
-		<GroceryList list={['vegetables', 'grains', 'hot sauce']}/>
-	</div>	
-);
-
 class GroceryListItem extends React.Component {
 	constructor(props) {
-	  super(props); 
-	  this.state = {
-		  done: false
-	  };
+		super(props);
+
+		this.state = {
+			done: false,
+			hovered: false
+		};
 	}
 	onListItemClick() {
 		this.setState({
 			done: !this.state.done
 		});
 	}
-
-	render() {
-	  var style = {
-		  textDecoration: this.state.done ? 'line-through' : 'none'
-	  };  
-	  return (
-		<li key={this.props.key} className='groceryList' style={style} onClick={this.onListItemClick.bind(this)} >{this.props.list}</li>
-	  );
+	onMouseOver() {
+		this.setState({
+			hovered: true
+		})
 	}
-  };
+	onMouseOut() {
+		this.setState({
+			hovered: false
+		})
+	}
+	render() {
+		var css = {
+			textDecoration: this.state.done ? 'line-through' : 'none',
+			fontWeight: this.state.hovered ? 'bold' : 'normal'
+		};
+		return (
+			<li style={css} onClick={this.onListItemClick.bind(this)} onMouseOut={this.onMouseOut.bind(this)}
+				onMouseOver={this.onMouseOver.bind(this)}>{this.props.item}</li>
+		);
+	};
+};
+const GroceryListItems = (props) => (
+	<ul>
+		{props.list.map((item, index) => <GroceryListItem
+			item={item} key={index} />
+		)}
+	</ul>
 
-ReactDOM.render(<App />, 
-	document.getElementById('app'));
+);
+const GroceryList = () => (
+	<div>
+		<h1>Grocery List</h1>
+		<GroceryListItems list={['Cucumbers', 'Tomatoes', 'Spinach', 'Sourdough Bread', 'Rice']} />
+	</div>
+);
+
+ReactDOM.render(<GroceryList />, document.getElementById("app"));
